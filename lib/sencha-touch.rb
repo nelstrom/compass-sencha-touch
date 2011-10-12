56338905932c3d0fca6fb01ee5b6797166fbb509
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-12 17:31:54 +0100

    Require the theme_images module.

--- lib/sencha-touch.rb
+++ lib/sencha-touch.rb
@@ -1,3 +1,4 @@
-require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+require 'compass'
+require File.join(extension_path, 'lib', 'theme_images.rb')
 Compass::Frameworks.register('sencha-touch', :path => extension_path)
